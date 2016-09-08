bd7fd1aa4a8e4575b8374992045b3aede5ae263c
Michael Huang <mike@htwins.net>
2016-09-08 15:30:32 -0700

    Add `build-client-watch` task

--- client-webpack.config.js
+++ client-webpack.config.js
@@ -1,12 +1,16 @@
 // Client
 var webpack = require('webpack');
-var tsconfig = './client-building/tsconfig.json';
+var tsconfig = './client-tsconfig.json';
 module.exports = {
     entry: './src/client/client.ts',
     output: {
         filename: './web/poppin.js'
     },
     devtool: 'source-map',
+    externals: {
+        'firebase': 'firebase',
+        'd3': 'd3'
+    },
     resolve: {
         // Add `.ts` and `.tsx` as a resolvable extension.
         extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
@@ -23,7 +27,7 @@ module.exports = {
             // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
             {
                 test: /\.tsx?$/,
-                loader: 'ts-loader?configFileName=' + tsconfig
+                loader: 'ts-loader?transpileOnly=true&configFileName=' + tsconfig
             }
         ]
     }
